6d3fb8203b433e0097a34a9d7c6f9e8559c0ff5e
Ahmed Omar <ahmedomar@hey.com>
2022-03-01 16:36:38 +0000

    added some JS functionality

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 # todo-list-app
+
+This is a prototype Todo application for a small business, that I created using HTML, CSS and JavaScript.
+
+The Design:
+
+<img src="./images/todo.png">
+
+The Code:
+
+<img src="./images/todo-code.png">
--- script.js
+++ script.js
@@ -7,7 +7,12 @@ const resetButton = document.querySelector("#resetbtn");
 //  Call all event listeners
 allEventListeners();
 
-// Functions of all event listners
+// Functions of all event listeners
+/*
+1. Add todo event
+2. Remove and complete todo event
+3. Clear or remove all todos
+*/
 function allEventListeners() {
   // Add todo event
   form.addEventListener("submit", addTodo);
@@ -15,10 +20,21 @@ function allEventListeners() {
   todoList.addEventListener("click", removeTodo);
   // Clear or remove all todos
   resetButton.addEventListener("click", clearTodoList);
-  // Search todo event
 }
 
 // Adding todo item function
+/*
+1. Create a new li element
+2. Add class
+3. Add complete and remove icon
+4. Create a new span element
+5. Add class
+6. Create a new text node and append to span
+7. Append span to li
+8. Append li to ul (todoList)
+9. Clear input
+10. Prevent default action
+*/
 function addTodo(e) {
   if (todoInput.value != "") {
     // Create li element
@@ -50,6 +66,11 @@ function addTodo(e) {
 }
 
 // Remove and complete todo item function
+/*
+1. I'm using the event listener on the todo list to listen for a click on the trash icon.
+2. If the user clicks on the trash icon, we’ll show a confirmation dialog.
+3. If the user confirms the action, we’ll remove the todo from the list.
+*/
 function removeTodo(e) {
   // Remove todo
   if (e.target.classList.contains("trash")) {
@@ -59,6 +80,10 @@ function removeTodo(e) {
   }
 
   // Complete todo
+  /*
+  1. When the user clicks on the text of the todo item, the class of the parent element is toggled.
+  2. When the user clicks on the icon of the todo item, the class of the parent element is toggled.
+  */
   if (e.target.classList.contains("todo-text")) {
     e.target.parentElement.classList.toggle("done");
   }
